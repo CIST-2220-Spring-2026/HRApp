4e95a9d7384b575fd5e9d1e589593000ca4247e1
Robert Garner <rgarner011235@gmail.com>
2026-03-25 13:15:56 -0600

    Add project files.

---
+++ HRApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HRApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HRApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace HRApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string text;
+        public string Text 
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
